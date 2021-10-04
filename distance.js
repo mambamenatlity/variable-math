7563fb870e4fcc48bb588a4f74839ed0e80798b6
Gerald Buchanan <geraldbuchanan@Geralds-MacBook-Pro.local>
2021-10-03 23:47:05 -0400

    mathmath

---
+++ distance.js
@@ -0,0 +1,10 @@
+const x1 = prompt('Please enter x1: ');
+const y2 = prompt('Please enter y1: ');
+const x2 = prompt('Please enter x2: ');
+const y2 = prompt('Please enter y2: ');
+
+const coordum = dx**2 + dy**2;
+
+const distance = ((x2 - x1) ** 2 + (y2 - y1) ** 0.5;
+
+console.log('Distance is' , distance);
---
+++ types.js
@@ -0,0 +1,29 @@
+const prompt = require('prompt-sync')
+
+const str = 'words and things like that'
+
+const num = 12312;
+
+console.log(str. num);
+
+// ============
+
+const x = '10';
+const y = '10';
+console.log(x + y);
+
+// ============
+
+const oursecretvariable = 'hello';
+
+console.log(type of our Sercretvariable);
+
+console.log(typeof input);
+
+// ============
+
+const input = prompt('Enter Anything: ');
+
+console.log('Your value is of type', typeof input);
+
+console.log('Your value is:', input)
